fc70edb38c70d4a147ee1dd89f8bb48741d49694
Jackson Herbert <jherbe3@gmu.edu>
2026-09-15 23:33:23 -0400

    T1.5: terraform backend s3

--- infra/versions.tf
+++ infra/versions.tf
@@ -7,4 +7,12 @@ terraform {
       version = "~> 6.0"
     }
   }
+
+  backend "s3" {
+    bucket       = "splitly-tfstate-725423737107"
+    key          = "splitly/terraform.tfstate"
+    region       = "us-east-1"
+    encrypt      = true
+    use_lockfile = true
+  }
 }
