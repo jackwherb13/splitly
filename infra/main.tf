d2efdd80888eb262db29372d83f3a674a717b9a7
Jackson Herbert <jherbe3@gmu.edu>
2026-09-15 23:24:05 -0400

    T1.5: terraform bucket

--- infra/main.tf
+++ infra/main.tf
@@ -1,3 +1,37 @@
 provider "aws" {
   region = "us-east-1"
 }
+
+resource "aws_s3_bucket" "state" {
+  bucket = "splitly-tfstate-725423737107"
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "aws_s3_bucket_versioning" "state" {
+  bucket = aws_s3_bucket.state.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "state" {
+  bucket = aws_s3_bucket.state.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "state" {
+  bucket                  = aws_s3_bucket.state.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
